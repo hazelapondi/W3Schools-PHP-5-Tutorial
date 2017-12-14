a8775efc18570db8040977d7d92658b6b15baba4
Hazel Apondi <apondihazel@gmail.com>
2017-12-14 12:11:36 +0300

    Update conditional.php

--- conditional.php
+++ conditional.php
@@ -8,6 +8,34 @@
   if ($t < "20") {
     echo "Have a good day!";
   }
+    
+    $s = date("H");
+
+  if ($s < "10") {
+    echo "Have a good morning!";
+  } elseif ($s < "20") {
+    echo "Have a great day!";
+  } else {
+    echo "Have a good night!";
+  }
+
+  $favcolor = "red";
+
+  switch ($favcolor) {
+    case "red":
+      echo "Your favorite color is red!";
+      break;
+    case "blue":
+      echo "Your favorite color is blue!";
+      break;
+    case "green":
+      echo "Your favorite color is green!";
+      break;
+
+    default:
+      echo "Your favorite color is neither red, blue nor green!";
+      break;
+  }  
   ?>
   </body>
 </html>
