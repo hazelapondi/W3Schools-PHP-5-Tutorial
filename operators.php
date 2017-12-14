1310799f34145e16ed36e1cee023a5b945f7ec46
Hazel Apondi <apondihazel@gmail.com>
2017-12-14 12:01:15 +0300

    Create operators.php

---
+++ operators.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+  <body>
+    <?php
+    $a = 12;
+    $b = 8;
+
+    echo ++$a; //pre-increment increments $a by one then returns $a
+    echo $a++; //post-increment returns $a then increments $a by one
+    echo --$b; //pre-decrement
+    echo $b--; //post-decrement
+    ?>
+  </body>
+</html>
