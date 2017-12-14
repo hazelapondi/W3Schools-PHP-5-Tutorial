3050118498da17e0ddea3726a574b5c64619c411
Hazel Apondi <apondihazel@gmail.com>
2017-12-14 11:58:06 +0300

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <body>
+
+    <?php
+    $x = 32 * 10;
+    echo $x;
+
+    $color = "red";
+    echo "My car is  $color. ";
+
+    $txt = "Ola!";
+    $a = 5;
+    $b = 10.5;
+    echo $txt;
+    echo $a;
+    echo $b;
+    ?>
+
+  </body>
+</html>
