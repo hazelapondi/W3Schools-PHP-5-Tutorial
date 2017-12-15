0ce6440c8e2fed77dab2ef44bebde1613d30f62f
Hazel Apondi <apondihazel@gmail.com>
2017-12-15 11:36:34 +0300

    Update simpleForm.php

--- simpleForm.php
+++ simpleForm.php
@@ -8,4 +8,8 @@
         <input type="submit">
       </form>
   </body>
+  <!--
+  NB:Ensure to validate form data to protect your script from malicious code.
+  Proper validation of form data is important to protect your form from hackers and spammers!
+  -->
 </html>
